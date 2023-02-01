e2a722a2037eb5d245c525bc41812f479304d127
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-01 14:47:09 -0300

    exercise 3

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,29 @@
-function MaxValue (shares) {
+function MaxValueOld(shares) {
   // Your code here:
+  let abs = 0
+  for (let i = 0; i < shares.length; i++) {
+    let max = 0
+    for (let j = i; j < shares.length; j++) {
+      shares[j] > shares[i] && shares[j] > max
+        ? max = shares[j] : null
+    }
+    const diff = max - shares[i]
+    diff > abs ? abs = diff : null
+  }
 
+  return abs
+}
+
+function MaxValue(arr) {
+  let minPrecio = arr[0]
+  let maxDif = -Infinity
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - minPrecio > maxDif) maxDif = arr[i] - minPrecio
+    if (arr[i] < minPrecio) minPrecio = arr[i]
+  }
+
+  return maxDif
 }
 
 module.exports = MaxValue
--- Tests/03-MaxValue.spec.js
+++ Tests/03-MaxValue.spec.js
@@ -1,11 +1,14 @@
 const { expect } = require('chai')
 const MaxValue = require('../Ejercicios/03-MaxValue/MaxValue')
 
-xdescribe('MaxValue', function () {
-  it('should return 9 if the array of shares is [4,3,2,5,11]', function () {
+describe('MaxValue', function () {
+  it('should return 9 if the array of shares is [4, 3, 2, 5, 11]', function () {
     expect(MaxValue([4, 3, 2, 5, 11])).to.equal(9)
   })
-  it('should return 9 if the array of shares is [23,7,3,4,8,6]', function () {
+  it('should return 5 if the array of shares is [23, 7, 3, 4, 8, 6]', function () {
     expect(MaxValue([23, 7, 3, 4, 8, 6])).to.equal(5)
+  })
+  it('should return 25 if the array of shares is [5, 15, 30, 1, 2]', function () {
+    expect(MaxValue([5, 15, 30, 1, 2])).to.equal(25)
   })
 })
