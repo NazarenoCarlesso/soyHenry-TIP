1229c56d2b20cad57c8f0c97393578b0ed8f7a98
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-02 14:46:41 -0300

    exercise 4

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,29 @@
-function mdArraySum (arr) {
-  // Your code here:
+const { arrayBuffer } = require("stream/consumers")
 
+function mdArray(arr) {
+  // Your code here:
+  let sum = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) sum += mdArraySum(arr[i])
+    else sum += arr[i]
+  }
+
+  return sum
 }
+
+function mdArraySum(arr) {
+  // Your code here:
+  if (!Array.isArray(arr)) return arr
+  let sum = 0
+  arr.forEach(a => sum += mdArraySum(a))
+  return sum
+}
+
+function mdArrayReduce(arr) {
+  // Your code here:
+  return arr.reduce((acc, current) => acc + (Array.isArray(current) ? mdArraySum(current) : current), 0)
+}
+
 
 module.exports = mdArraySum
