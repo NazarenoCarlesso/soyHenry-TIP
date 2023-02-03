7b525aa22e60de0e4b6d5dc5c72e82fdf7ac4e34
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-03 15:06:09 -0300

    exercise 5

--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,11 +1,44 @@
 class Node {
   // Your code here:
-
+  constructor(value) {
+    this.next = null
+    this.value = value
+    this.min = null
+  }
 }
 
 class MinStack {
   // Your code here:
+  constructor() {
+    this.top = null
+  }
+}
 
+MinStack.prototype.push = function (value) {
+  if (!this.top) {
+    this.top = new Node(value)
+    this.top.min = this.top.value
+  } else {
+    const newTop = new Node(value)
+    value < this.top.min ? (newTop.min = value) : newTop.min = this.top.min
+    newTop.next = this.top
+    this.top = newTop
+  }
+}
+
+MinStack.prototype.pop = function () {
+  let value
+  this.top ? (value = this.top.value) : null
+  this.top ? (this.top = this.top.next) : null
+  return value
+}
+
+MinStack.prototype.peek = function () {
+  return this.top.value
+}
+
+MinStack.prototype.min = function () {
+  return this.top.min
 }
 
 module.exports = {
