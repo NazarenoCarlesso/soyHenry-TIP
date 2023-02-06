cb7e751be6c7582d17a74d270f8872a6f30d3a33
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-06 14:25:05 -0300

    exercise 6

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,19 @@
-function HasBalancedBrackets (string) {
+function HasBalancedBrackets(string) {
   // Your code here:
+  let valid = true
+  const op = ['(', '[', '{']
+  const end = [')', ']', '}']
+  const stack = []
 
+  string.split('').forEach(b => {
+    if (['(', '[', '{'].includes(b)) stack.push(b)
+    if ([')', ']', '}'].includes(b)) {
+      if (op.indexOf(stack.pop()) !== end.indexOf(b)) valid = false
+    }
+  })
+  if (stack.length > 0) valid = false
+
+  return valid
 }
 
 module.exports = HasBalancedBrackets
