b29af821d311a27cae4c6fc7ac8926d1f95e6abb
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-08 14:48:32 -0300

    exercise 8

--- Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
@@ -1,6 +1,17 @@
-function FindWordStartingWith (book, query) {
+function FindWordStartingWith(book, query) {
+  const regex = new RegExp(`^${query}`)
   // Your code here:
+  book = book.text.toLowerCase()
+  let lastIndex = 0
+  let indexes = []
 
+  book.split(' ').filter(w => regex.test(w)).forEach(w => {
+    indexes.push(book.indexOf(w, lastIndex))
+    lastIndex = book.indexOf(w, lastIndex)
+    lastIndex++
+  })
+  // return book.split(' ').filter(w => regex.test(w)).map(w => book.indexOf(w))
+  return indexes
 }
 
 module.exports = FindWordStartingWith
