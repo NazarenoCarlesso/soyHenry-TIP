687e842915be23fac34788fb995b50659d144bcd
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-10 14:32:12 -0300

    exercise 10

--- Ejercicios/10-DecimalToBynary/DecimalToBynary.js
+++ Ejercicios/10-DecimalToBynary/DecimalToBynary.js
@@ -1,6 +1,13 @@
-function DecimalToBynary (num) {
+function DecimalToBynary(num) {
   // Your code here:
-
+  let binary = ''
+  // mientras sea mayor que cero
+  while (num) {
+    binary = num % 2 + binary
+    num = Math.floor(num / 2)
+  }
+  // devolvemos el string BINARY
+  return binary
 }
 
 module.exports = DecimalToBynary
