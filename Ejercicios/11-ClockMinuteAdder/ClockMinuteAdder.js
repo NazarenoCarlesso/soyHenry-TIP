ddb8986e513e47238615ad4cf4d41b8c52820a49
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-13 21:05:34 -0300

    exercise 11

--- Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
+++ Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,6 +1,17 @@
-function clockMinuteAdder (time, minutesToAdd) {
+function clockMinuteAdder(time, minutesToAdd) {
   // Your code here:
-
+  // desestructuracion del tiempo
+  let [hours, minutes] = time.split(':')
+  minutes = Number(minutes) + minutesToAdd
+  // mientras los minutos sean mas de 60
+  while (minutes >= 60) {
+    hours = Number(hours) + 1
+    minutes -= 60
+  }
+  // mientras las horas sean mas de 12
+  while (hours > 12) hours -= 12
+  // formato de 2 digitos
+  return `${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}`
 }
 
 module.exports = clockMinuteAdder
