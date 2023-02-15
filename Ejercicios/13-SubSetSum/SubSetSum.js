522237c9601ae704ea2a156171e6b165d7e6e1c8
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-15 17:30:10 -0300

    exercise 13

--- Ejercicios/13-SubSetSum/SubSetSum.js
+++ Ejercicios/13-SubSetSum/SubSetSum.js
@@ -1,6 +1,11 @@
-function subsetSum (nums, n) {
+function subsetSum (nums, target, index = 0) {
   // Your code here:
-
+  if (target === 0) return true
+  if (target < 0) return false
+  if (index > nums.length) return false
+  const whenExcluded = subsetSum(nums, target, index + 1)
+  const whenIncluded = subsetSum(nums, target - nums[index], index + 1)
+  return whenExcluded || whenIncluded
 }
 
 module.exports = subsetSum
