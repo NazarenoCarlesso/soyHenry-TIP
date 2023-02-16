54432098bafb4d602aa99c2dd2174dccf13d8ec9
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-02-16 16:01:56 -0300

    exercise 15

--- Ejercicios/15-SpyOn/SpyOn.js
+++ Ejercicios/15-SpyOn/SpyOn.js
@@ -1,6 +1,24 @@
-function spyOn (fn) {
+function spyOn(fn) {
   // Your code here:
+  let callCount = 0
+  const calledWith = new Map()
+  const returned = new Map()
 
+  const spy = (...args) => {
+    const result = fn(...args)
+    callCount++
+    for (const arg of args) calledWith.set(arg, true)
+
+    returned.set(result, true)
+
+    return result
+  }
+
+  spy.getCallCount = () => callCount
+  spy.wasCalledWith = (value) => !!calledWith.get(value)
+  spy.returned = (value) => !!returned.get(value)
+
+  return spy
 }
 
 module.exports = spyOn
